6db756d85dd7efe42c8f838933791551a841bce9
Chase McClellan <mcclel54@uwm.edu>
2024-11-26 02:55:05 -0600

    Add project files.

---
+++ FirstCudaProject/kernel.cu
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+__global__ void helloCUDA() {
+    printf("Hello from the GPU thread %d\n", threadIdx.x);
+}
+
+int main() {
+    // Launch kernel
+    helloCUDA << <1, 10 >> > ();
+    // Wait for GPU to finish
+    cudaDeviceSynchronize();
+
+    return 0;
+}
